cb5d3e496cc53924ee9ac133e0010614b992a0b7
Ignat Insarov <kindaro@gmail.com>
2019-10-07 19:04:40 +0300

    Hang Rasterific.

--- Main.hs
+++ Main.hs
@@ -1,19 +1,15 @@
 module Main where
 
 import qualified Data.List as List
+import Data.Bifunctor
 import Diagrams.Prelude
-import Diagrams.Backend.SVG.CmdLine
+import Diagrams.Backend.Rasterific.CmdLine
 import Data.Graph.Inductive.Example
 
 import qualified Layout
 import Draw
 
 main :: IO ()
-main = mainWith @(Diagram B) $ List.foldl1' (|||) graphs
-  where
-    graphs = fmap ((square 2.2 <>) . draw) $
-        [ Layout.random kin248
-        , Layout.circular kin248
-        , Layout.random dag4
-        , Layout.circular dag4
-        ]
+main = mainWith @(Diagram B)
+     $ (square 2.2 <>) . draw . Layout.circular . bimap (const 1) id
+     $ clr489
