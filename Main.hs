1d4f12d2bd27aadd9c4a80ea1c73f4b0cbead8ad
Ignat Insarov <kindaro@gmail.com>
2019-10-28 12:22:33 +0300

    Classify and draw graphs.

--- Main.hs
+++ Main.hs
@@ -1,19 +1,64 @@
 module Main where
 
-import qualified Data.List as List
+import qualified Data.List       as List
+import qualified Data.List.Split as List
+import Data.Bifunctor
+import Data.Bitraversable
 import Diagrams.Prelude
 import Diagrams.Backend.SVG.CmdLine
-import Data.Graph.Inductive.Example
+import Data.Graph.Inductive (DynGraph, Gr, Node)
+import qualified Data.Graph.Inductive.Example as E
 
 import qualified Layout
 import Draw
+import Analyze
+import Instances
+
+data AnyGraph = forall gr a b.
+              (DynGraph gr, Bicontainer gr, Bitraversable gr, Eq (gr a b), IndexL gr ~ Int)
+              => AnyGraph { getSomeGraph :: gr a b }
+
+
+examples :: [AnyGraph]
+examples = [ AnyGraph E.a, AnyGraph E.b, AnyGraph E.clr486, AnyGraph E.clr528, AnyGraph E.d3
+           , AnyGraph E.e, AnyGraph E.loop
+           , AnyGraph E.vor, AnyGraph E.ab, AnyGraph E.c, AnyGraph E.clr489, AnyGraph E.clr595
+           , AnyGraph E.dag3, AnyGraph E.e3, AnyGraph E.gr1
+           , AnyGraph E.cyc3
+           , AnyGraph E.kin248, AnyGraph (E.star @Gr 13)
+           , AnyGraph E.abb, AnyGraph E.clr479, AnyGraph E.clr508, AnyGraph E.d1
+           , AnyGraph E.dag4, AnyGraph E.g3
+           , AnyGraph E.g3b, AnyGraph (E.ucycle @Gr 13)
+           ]
+
+niceExamples :: [AnyGraph]
+niceExamples = [ AnyGraph E.clr486, AnyGraph E.clr528, AnyGraph E.d3
+           , AnyGraph E.e, AnyGraph E.loop
+           , AnyGraph E.vor, AnyGraph E.ab, AnyGraph E.clr489, AnyGraph E.clr595
+           , AnyGraph E.dag3, AnyGraph E.e3, AnyGraph E.gr1
+           , AnyGraph E.cyc3
+           , AnyGraph E.kin248, AnyGraph (E.star @Gr 13)
+           , AnyGraph E.abb, AnyGraph E.clr479, AnyGraph E.clr508, AnyGraph E.d1
+           , AnyGraph E.dag4, AnyGraph E.g3
+           , AnyGraph E.g3b, AnyGraph (E.ucycle @Gr 13)
+           ]
 
 main :: IO ()
-main = mainWith @(Diagram B) $ List.foldl1' (|||) graphs
+main = mainWith @(Diagram B)
+     $ (tile . fmap renderOne) examples
+
+renderOne :: AnyGraph -> Diagram B
+renderOne (AnyGraph gr) = gr & Layout.circular
+                             & draw
+                             & decorate gr
+                             & frame
   where
-    graphs = fmap ((square 2.2 <>) . draw) $
-        [ Layout.random kin248
-        , Layout.circular kin248
-        , Layout.random dag4
-        , Layout.circular dag4
-        ]
+    decorate gr' x = if isAcyclic gr'
+                        then x <> (circle 0.1 & fc green & translate (r2 (0.95, 0.95)))
+                        else x
+
+    frame d = d <> (square 2.4 & lcA (black `withOpacity` 0)) <> square 2.2
+
+tile :: [Diagram B] -> Diagram B
+tile xs = let columns = (ceiling . sqrt . fromIntegral . length) xs
+          in (vcat . fmap hcat . List.chunksOf columns) xs
--- src/Analyze.hs
+++ src/Analyze.hs
@@ -1,6 +1,35 @@
 module Analyze where
 
 import Data.Graph.Inductive
+import Instances
+
+converge :: Eq a => [a] -> [a]
+converge = convergeBy (==)
+
+fixp :: Eq a => (a -> a) -> a -> a
+fixp f = last . converge . iterate f
+
+convergeBy :: (a -> a -> Bool) -> [a] -> [a]
+convergeBy _ [ ] = [ ]
+convergeBy _ [x] = [x]
+convergeBy eq (x: xs@(y: _))
+    | x `eq` y = [x]
+    | otherwise = x : convergeBy eq xs
+
+fixpBy :: (a -> a -> Bool) -> (a -> a) -> a -> a
+fixpBy eq f = last . convergeBy eq . iterate f
 
 isPlanar :: Graph gr => gr a b -> Bool
 isPlanar _ = undefined
+
+isAcyclic :: (DynGraph gr, Eq (gr a b)) => gr a b -> Bool
+isAcyclic gr = case (fixp tearLeaves) gr of
+    Empty -> True
+    _     -> False
+
+tearLeaves :: DynGraph gr => gr a b -> gr a b
+tearLeaves gr = let f x = if isLeaf x then id else (x:) in subgraph ((fmap node' . ufold f [ ]) gr) gr
+
+isLeaf :: Context a b -> Bool
+isLeaf (_, _, _, [ ]) = True
+isLeaf (_, _, _, _  ) = False
