ef597de062d44186070d2977ce60164ea3544e8f
Ignat Insarov <kindaro@gmail.com>
2019-11-06 19:32:13 +0300

    Factor out Converge.

--- src/Analyze.hs
+++ src/Analyze.hs
@@ -3,22 +3,7 @@ module Analyze where
 import Prelude
 import Data.Graph.Inductive
 import Instances
-
-converge :: Eq a => [a] -> [a]
-converge = convergeBy (==)
-
-fixp :: Eq a => (a -> a) -> a -> a
-fixp f = last . converge . iterate f
-
-convergeBy :: (a -> a -> Bool) -> [a] -> [a]
-convergeBy _ [ ] = [ ]
-convergeBy _ [x] = [x]
-convergeBy eq (x: xs@(y: _))
-    | x `eq` y = [x]
-    | otherwise = x : convergeBy eq xs
-
-fixpBy :: (a -> a -> Bool) -> (a -> a) -> a -> a
-fixpBy eq f = last . convergeBy eq . iterate f
+import Converge
 
 isPlanar :: Graph gr => gr a b -> Bool
 isPlanar _ = undefined
---
+++ src/Converge.hs
@@ -0,0 +1,19 @@
+module Converge where
+
+import Prelude
+
+converge :: Eq a => [a] -> [a]
+converge = convergeBy (==)
+
+fixp :: Eq a => (a -> a) -> a -> a
+fixp f = last . converge . iterate f
+
+convergeBy :: (a -> a -> Bool) -> [a] -> [a]
+convergeBy _ [ ] = [ ]
+convergeBy _ [x] = [x]
+convergeBy eq (x: xs@(y: _))
+    | x `eq` y = [x]
+    | otherwise = x : convergeBy eq xs
+
+fixpBy :: (a -> a -> Bool) -> (a -> a) -> a -> a
+fixpBy eq f = last . convergeBy eq . iterate f
