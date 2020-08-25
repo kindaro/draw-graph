94230aa69a2d51662667333e17ade60da8a1b8ee
Ignat Insarov <kindaro@gmail.com>
2020-08-25 08:45:00 +0500

    Add check for unordered pair construction.

--- checks/properties/Main.hs
+++ checks/properties/Main.hs
@@ -11,4 +11,5 @@ import Draw
 main ∷ IO ( )
 main = defaultMain $ testGroup "Properties."
   [ testProperty "Edges can be contracted and expanded" $ (expandEdges @Gr @Int @Int) `inverts` compactifyEdges
+  , testProperty "Unordered pair pattern is correct." $ u2ToV2 . uncurry (U2 @Int) <=> u2ToV2 . uncurry (flip U2)
   ]
